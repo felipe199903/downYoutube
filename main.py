d59c5accf5fb76d2bb2f1107d7ec1ba5d8e34012
Felipe Schorles <felipeschorles@outlook.com>
2022-12-07 11:50:30 -0300

    feature/download-out

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def download(link):
     youtube_object = youtube_object.streams.get_highest_resolution()
 
     try:
-        youtube_object.download()
+        youtube_object.download(output_path='download')
     except TypeError as e:
         raise TypeError("There has been an error in downloading your youtube video") from e
 
