aee48ba1409678617f37515b38118edf43b6dbe9
Felipe Schorles <felipeschorles@outlook.com>
2022-12-07 11:49:30 -0300

    feature/env

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from pytube import YouTube
 
+
 def download(link):
-  youtube_object = YouTube(link)
-  youtube_object = youtube_object.streams.get_highest_resolution()
+    youtube_object = YouTube(link)
+    youtube_object = youtube_object.streams.get_highest_resolution()
 
-  try:
-      youtube_object.download()
-  except TypeError as e:
-    raise TypeError("There has been an error in downloading your youtube video") from e
+    try:
+        youtube_object.download()
+    except TypeError as e:
+        raise TypeError("There has been an error in downloading your youtube video") from e
 
-  print("Download has completed!")
+    print("Download has completed!")
+
 
 link = input("Youtube URL: ")
 download(link)
